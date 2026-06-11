72a0c63b8049da9743deb417e8f91588afaa5400
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2026-06-11 15:53:18 +0200

    init

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Landing() {
   return (
-    <View className="flex-1 items-center justify-center bg-surface">
-      <Text className="text-foreground text-xl">Pickup</Text>
+    <View className="flex-1 items-center justify-center bg-primary">
+      <Text className="text-defaulttext text-x2">Pickup</Text>
     </View>
   );
 }
