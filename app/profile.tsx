f0dba10ef92464cba40be5f19570cc34e08bbec8
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2026-06-17 16:23:05 +0200

    page scaffolding

---
+++ app/profile.tsx
@@ -0,0 +1,10 @@
+import { PageHeader } from "../components/typography/Typography";
+import PageContent from "../components/layout/PageContent";
+
+export default function Profile() {
+  return (
+    <PageContent className="items-start justify-start gap-8">
+      <PageHeader>profile</PageHeader>
+    </PageContent>
+  );
+}
